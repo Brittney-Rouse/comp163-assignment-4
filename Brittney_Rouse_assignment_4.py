a685e6bb0dcbb79a4b2f94a31048f2f2777d734f
Brittney-Rouse <barouse1@aggies.ncat.edu>
2025-09-23 12:31:35 -0400

    Add files via upload

---
+++ Brittney_Rouse_assignment_4.py
@@ -0,0 +1,12 @@
+student_name = "Brittney Rouse"
+current_gpa = 4.0
+study_hours = 20
+social_points = 72
+stress_level = 30
+
+print(f"Student Name: {student_name}")
+print(f"Starting Stats:")
+print(f"Current GPA: {current_gpa}")
+print(f"Study Hours: {study_hours}")
+print(f"Social Points: {social_points}")
+print(f"Stress Level: {stress_level}")
